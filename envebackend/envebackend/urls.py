69ecf3a6038af47e498ab112828dabd585211a31
ehernandep <ehernandep@eafit.edu.co>
2023-03-21 22:54:16 -0500

    add custom user model

--- envebackend/envebackend/urls.py
+++ envebackend/envebackend/urls.py
@@ -6,3 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('secret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Enve Backend Admin"
+admin.site.site_title = "Enve Backend Admin Portal "
+admin.site.index_title = "Welcome To Enve"
