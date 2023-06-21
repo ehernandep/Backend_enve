3653023813fc3ae2d4c5dca22a04e07b0cec73b3
ehernandep <ehernandep@eafit.edu.co>
2023-06-20 23:35:00 -0500

    a lot of changes

--- agent/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from core.models import Listing
-
-
-class ListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Listing
-        fields = '__all__'
-    
--- agent/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import ListingAPIView
-
-urlpatterns = [
-    path('', include('common.urls')),
-    path('listings', ListingAPIView.as_view()),
-
-]
